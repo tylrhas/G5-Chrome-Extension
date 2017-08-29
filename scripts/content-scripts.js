ff423c5e3083d80ec0ab080a257698cb2ff4e025
Tylrhas <tylrhas@gmail.com>
2017-08-29 08:34:00 -0700

    Removal of alert from script

--- scripts/content-scripts.js
+++ scripts/content-scripts.js
@@ -1,5 +1,4 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  alert('we here');
       if (msg.action == 'open_dialog_box') {
         var s = document.createElement('script');
         s.src = chrome.extension.getURL('scripts/wysiwyg-script.js');
