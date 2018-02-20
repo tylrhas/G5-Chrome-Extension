cb5510287a84f33805506d1c4fe69c12ee897a49
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-02-20 14:46:57 -0800

    chatmeter CSV

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function hub_poi() {
   });
 }
 
-function googleCID(){
+function googleCID() {
   chrome.tabs.executeScript({ file: "scripts/jquery-3.2.1.min.js" }, function () {
     chrome.tabs.executeScript({ file: "functions/googleCID.js" });
   });
@@ -32,7 +32,7 @@ function openHub() {
     });
   });
 }
-function autoalt(){
+function autoalt() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "autoalt" }, function (response) {
       console.log(response);
@@ -80,11 +80,11 @@ function transfer() {
   });
 }
 
-function changelogs(){
+function changelogs() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentURL = tabs[0].url;
     splitURL = currentURL.split('/');
-    changelogsURL = 'https://'+splitURL[2]+'/api/websites/'+splitURL[4]+'/changelogs';
+    changelogsURL = 'https://' + splitURL[2] + '/api/websites/' + splitURL[4] + '/changelogs';
     console.log(changelogsURL);
     chrome.tabs.create({ url: changelogsURL });
   });
@@ -107,28 +107,122 @@ function createQueryStringObj(header, locationData) {
 }
 function createNewTab(obj) {
   //if (!delete obj.urn) { throw new Error(); }
-   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentURL = tabs[0].url;
-    var createNewTab = currentURL+"/"+obj.urn+"/edit?"+createQueryString(obj);
+    var createNewTab = currentURL + "/" + obj.urn + "/edit?" + createQueryString(obj);
     console.log(createNewTab);
-    chrome.tabs.create({ url: createNewTab, active:false});
+    chrome.tabs.create({ url: createNewTab, active: false });
   });
 
 }
-  function createQueryString(obj) {
-    var str = [];
-    for (var p in obj)
-      if (obj.hasOwnProperty(p)) {
-        if(p != "urn"){
+function createQueryString(obj) {
+  var str = [];
+  for (var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      if (p != "urn") {
         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
       }
-      }
-    return str.join("&");
-  }
+    }
+  return str.join("&");
+}
 //end hub updater functions
-  function hubSearch(){
-    chrome.tabs.executeScript({ file: "functions/hub-Search.js" });
+function hubSearch() {
+  chrome.tabs.executeScript({ file: "functions/hub-Search.js" });
+}
+
+function chatmeterCSV() {
+  chrome.tabs.query({ active: true, currentWindow: true }, createChatmeterCSV);
+}
+
+function createChatmeterCSV(tabs) {
+  var currentURL = tabs[0].url;
+  var csv_object = []
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", currentURL + '.json', true);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.send();
+  xhr.onload = function () {
+    var data = JSON.parse(xhr.responseText);
+    if (xhr.status === 200) {
+      console.log(data);
+      if (data.domain_type == 'SingleDomainClient') {
+        client_domain = data.naked_domain
+        secure_domain = data.secure_domain
+        vertical = data.vertical.toLowerCase();
+      }
+      client_Name = data.branded_name;
+      //get the location information
+      var xhr2 = new XMLHttpRequest();
+      xhr2.open("GET", currentURL + '/locations.json', true);
+      xhr2.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      xhr2.send();
+      xhr2.onload = function () {
+        if (xhr.status === 200) {
+          var data = JSON.parse(xhr2.responseText);
+          for (i = 0; i < data.length; i++) {
+            if(data[i].status == "Pending"){
+              //location is pending
+            hub_location = {};
+            hub_location.ClientLocationId = data[i].urn
+            hub_location.ClientName = client_Name
+            hub_location.BusinessName = data[i].name
+            hub_location.street_address_1 = data[i].street_address_1
+            hub_location.Suite = data[i].street_address_2
+            hub_location.city = data[i].city
+            hub_location.state = data[i].state
+            hub_location.postal_code = data[i].postal_code
+            hub_location.Country = data[i].country
+            hub_location.Phone = data[i].phone_number
+            hub_location.AlternatePhone = ''
+            if (typeof client_domain === 'undefined') {
+              //multidomain
+              if (data[i].secure_domain) {
+                hub_location.WebsiteUrl = 'https://www.' + data[i].naked_domain
+              }
+              else {
+                hub_location.WebsiteUrl = 'http://www.' + data[i].naked_domain
+              }
+            }
+            else {
+              //singledomain
+              if (secure_domain) {
+                hub_location.WebsiteUrl = 'https://www.' + client_domain + '/' + vertical + '/' + data[i].city.toLowerCase() + '/' + data[i].state.toLowerCase() + '/' + custom_slug
+              }
+              else {
+                hub_location.WebsiteUrl = 'http://www.' + client_domain + '/' + vertical + '/' + data[i].city.toLowerCase() + '/' + data[i].state.toLowerCase() + '/' + custom_slug
+              }
+            }
+            hub_location.PrimaryCategory = ''
+            hub_location.AuditOnly = 'FALSE'
+            csv_object.push(hub_location);
+            console.log(csv_object);
+          }
+          }
+        }
+        //create CSV
+        csv = Papa.unparse(csv_object);
+        console.log(csv);
+        file = new Blob([csv], {type: "text/csv"});
+        url = URL.createObjectURL(file);
+        //date time for the file download
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;      // "+ 1" becouse the 1st month is 0
+        var day = date.getDate();
+        var hour = date.getHours();
+        var minutes = date.getMinutes();
+        var secconds = date.getSeconds()
+        console.log(file)
+        console.log(client_Name+'.csv');
+        chrome.downloads.download({
+          url: url,
+          filename : client_Name+'_'+month+'_'+day+'_'+year+'_'+hour+'_'+minutes+'_'+secconds+'.csv'
+          //saveAs : true
+        });
+      }
+    }
   }
+}
 
 //event listeners
 document.getElementById('hubpoi').addEventListener('click', hub_poi);
@@ -145,6 +239,7 @@ document.getElementById('search').addEventListener('click', hubSearch);
 document.getElementById('transfer').addEventListener('click', transfer);
 document.getElementById('changelogs').addEventListener('click', changelogs);
 document.getElementById('googleCID').addEventListener('click', googleCID);
+document.getElementById('chatmeterCSV').addEventListener('click', chatmeterCSV);
 
 
 //set the input value
@@ -162,9 +257,9 @@ $(function () {
     var poikey = $('#poikey').val();
     var transferURL = $('#transferURL').val();
     console.log(poikey);
-  
+
     // Save it using the Chrome extension storage API.
-    chrome.storage.sync.set({ 'poikey': poikey , 'transferURL' : transferURL}, function () {
+    chrome.storage.sync.set({ 'poikey': poikey, 'transferURL': transferURL }, function () {
       // Notify that we saved.
       console.log('Settings saved');
       $('#save').addClass('btn-success');
