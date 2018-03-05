0047cd799caac3936e5ab76c8c55b7b76c05d960
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-03-05 12:09:57 -0800

    fix POI bug

--- js/popup/hub_poi.js
+++ js/popup/hub_poi.js
@@ -1,6 +1,6 @@
 // @ts-check
 export function hub_poi() {
   chrome.tabs.executeScript({ file: "scripts/jquery-3.2.1.min.js" }, function () {
-    chrome.tabs.executeScript({ file: "functions/point_of_interest.js" });
+    chrome.tabs.executeScript({ file: "js/executable_scripts/point_of_interest.js" });
   });
 }
