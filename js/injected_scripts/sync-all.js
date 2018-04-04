e5cafed7edc84546df4b80c3161cf0a9901612ba
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-04-04 09:47:39 -0700

    fix non Parsed JSON

--- js/injected_scripts/sync-all.js
+++ js/injected_scripts/sync-all.js
@@ -3,6 +3,8 @@ $('#syncAll').click(function () {
   $('.table .btn').each(function (i , el) {
     var dataParams = $(el).attr('data-params')
     console.log(dataParams)
+    dataParams = JSON.parse(dataParams)
+    console.log(dataParams)
     var url = 'https://g5-hub.herokuapp.com/synchronizations?'
     var queryString = 'updatable_url' + '=' + encodeURIComponent(dataParams.updatable_url) + '&' + 'client_urn=' + dataParams.client_urn
     console.log(url + queryString)
